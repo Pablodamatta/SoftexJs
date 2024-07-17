e8f7a64da3cdefad6d094f7cf53164a0c2ebfcfd
PABLO ALMEIDA DA MATTA <PABLO3470@IOT>
2024-07-17 11:09:23 -0300

    primeiro commit

---
+++ ex1.js
@@ -0,0 +1,32 @@
+let number = [
+
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+
+
+]
+   console.log(number)
+//exercise 1 //
+
+
+let number2 = [
+    "0",
+    "1",
+    "3",
+    "4",
+
+]
+console.log(number2.reverse())
+//exercise2//
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,17 @@
+ let frutas = [
+    "maca",
+    "manga",
+    "morango",
+ ]
+ console.log(frutas[1]);
+ //fruit
+
+ let usuarios = [
+    "daniel",
+    "pablo",
+    "jose",
+
+
+
+ ]
+
